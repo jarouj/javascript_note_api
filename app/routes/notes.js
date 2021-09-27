8fb6c800bfa6b92c538d1d601dfc9ba9bba25ddd
jarouj <felipe.jarouj@gmail.com>
2021-09-27 20:25:31 -0300

    recurso para baixar a nota, com autenticação do proprietário

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const Note = require('../models/note');
+const withAuth = require('../middlewares/auth');
 
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   const { title, body } = req.body;
 
   try {
@@ -13,5 +14,25 @@ router.post('/', async (req, res) => {
     res.status(500).json({error: 'Problem to create a new note'});
   }
 })
+
+router.get('/:id', withAuth, async(req, res) => {
+  try {
+    const { id } = req.params;
+    let note = await Note.findById(id);
+    if(isOwner(req.user, note))
+      res.json(note);
+    else 
+      res.status(403).json({error: 'Permission denied'});
+  } catch (error) {
+    res.status(500).json({error: 'Problem to get a note'});
+  }
+})
+
+const isOwner = (user, note) => {
+  if(JSON.stringify(user._id) == JSON.stringify(note.author._id))
+    return true;
+  else
+    return false;
+}
 
 module.exports = router;
